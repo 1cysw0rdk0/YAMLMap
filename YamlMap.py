9fdb8a8f52ce522a5f26b94aa3ef16a20ef2062e
camdo <1cysw0rdk0@gmail.com>
2022-04-24 21:21:21 -0400

    update function structure

--- YamlMap.py
+++ YamlMap.py
@@ -30,6 +30,7 @@ def process_scripts(scripts):
         return None
     return "--script=" + ",".join(scripts)
 
+
 '''
 A few special cases allow for Nmap syntax for declaring all ports.
 Otherwise, join them with commas.
@@ -42,6 +43,9 @@ def process_ports(ports):
     return ports
 
 
+'''
+Process Script Arguments
+'''
 def process_script_args(args):
     return '--script-args="' + args.strip() + '"'
 
@@ -75,84 +79,88 @@ def process_defaults(defaults, scan_object):
     return scan_object
 
 
+'''
+Processes and executes a scan
+'''
+def process_scan(scan_name, conf, args):
+    # set up vars for this scan instance
+    default = False
+    scan = conf[scan_name]
+    scan_object = scanObject()
+    nmap = '/usr/bin/nmap'
+    target_name = args.targets.split(".")[0]
+
+    # default case flag
+    is_default_case = False
+    if scan_name == 'default':
+        is_default_case = True
+
+    # read in port, scripts and script args
+    # translate to scan_object attributes
+    try:
+        scan_object.scanAttributes['ports'] = process_ports(scan['ports'])
+    except KeyError:
+        pass
+
+    try:
+        scan_object.scanAttributes['scripts'] = process_scripts(scan['scripts'])
+    except KeyError:
+        scripts = None
+
+    # if no scripts, ignore args
+    if scripts is not None:
+        try:
+            scan_object.scanAttributes['script_args'] = process_script_args(scan['script_args'])
+        except KeyError:
+            pass
+
+    try:
+        scan_object.scanAttributes['misc'] = process_misc_args(scan['misc'])
+    except KeyError:
+        pass
+
+    # dont attempt to execute a default case
+    if is_default_case:
+        default = scan_object
+        return
+
+    # add the default settings to the current scan
+    if default:
+        scan_object = process_defaults(default, scan_object)
+
+    # more scan var setup
+    scan_type = "-" + scan['scan']
+    out_type = "-" + scan['out']
+    out_name = target_name + "_" + scan_name
+
+    # time stamp
+    # nmap already has a time stamp, may remove
+    cur_time = (time.strftime("%d %b %H:%M:%S")).upper()
+    print(cur_time + "> Kicking off '" + scan_name + "' Scan")
+
+    # begin subprocess cmd setup
+    cmd = [nmap, scan_type, out_type, out_name, "-iL", args.targets]
+    for attribute in scan_object.scanAttributes:
+        if type(scan_object.scanAttributes[attribute]) is list:
+            cmd.extend(scan_object.scanAttributes[attribute])
+        else:
+            cmd.append(scan_object.scanAttributes[attribute])
+
+    # Execute
+    # Print for testing
+    print(cur_time + "> " + ' '.join(cmd))
+    process = subprocess.run(cmd)
+
+
 def main():
 
     # Load in data
     args = handle_args()
     conf = yaml.safe_load(open(args.config))
 
-    # set up vars
-    nmap = '/usr/bin/nmap'
-    target_name = args.targets.split(".")[0]
-    default = False
-
     # Handle each scan
     for scan_name in conf:
-
-        # set up vars for this scan instance
-        scan = conf[scan_name]
-        scan_object = scanObject()
-
-        # default case flag
-        is_default_case = False
-        if scan_name == 'default':
-            is_default_case = True
-
-        # read in port, scripts and script args
-        # translate to scan_object attributes
-        try:
-            scan_object.scanAttributes['ports'] = process_ports(scan['ports'])
-        except KeyError:
-            pass
-
-        try:
-            scan_object.scanAttributes['scripts'] = process_scripts(scan['scripts'])
-        except KeyError:
-            scripts = None
-
-        # if no scripts, ignore args
-        if scripts is not None:
-            try:
-                scan_object.scanAttributes['script_args'] = process_script_args(scan['script_args'])
-            except KeyError:
-                pass
-
-        try:
-            scan_object.scanAttributes['misc'] = process_misc_args(scan['misc'])
-        except KeyError:
-            pass
-
-        # dont attempt to execute a default case
-        if is_default_case:
-            default = scan_object
-            continue
-
-        # add the default settings to the current scan
-        if default:
-            scan_object = process_defaults(default, scan_object)
-
-        # more scan var setup
-        scan_type = "-" + scan['scan']
-        out_type = "-" + scan['out']
-        out_name = target_name + "_" + scan_name
-
-        # time stamp
-        # nmap already has a time stamp, may remove
-        cur_time = (time.strftime("%d %b %H:%M:%S")).upper()
-        print(cur_time + "> Kicking off '" + scan_name + "' Scan")
-
-        # begin subprocess cmd setup
-        cmd = [nmap, scan_type, out_type, out_name, "-iL", args.targets]
-        for attribute in scan_object.scanAttributes:
-            if type(scan_object.scanAttributes[attribute]) is list:
-                cmd.extend(scan_object.scanAttributes[attribute])
-            else:
-                cmd.append(scan_object.scanAttributes[attribute])
-
-        # Execute
-        # Print for testing
-        print(cur_time + "> " + ' '.join(cmd))
-        process = subprocess.run(cmd)
+        process_scan(scan_name, conf, args)
 
 
 main()
